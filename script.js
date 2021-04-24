4b44173f11422b6c5570169afc271a9d88007d09
iptek-komtri <iptek-forkom-tri@gmail.com>
2021-04-24 20:46:34 +0700

    perubahan

--- script.js
+++
@@ -1,7 +0,0 @@
-<button type="button" class="btn btn-danger">Danger</button>
-const positif = document.createElement('button');
-positif.type = "button";
-positif.className = "btn btn-danger";
-positif.innerHTML = "Indonesia";
-const korona = document.querySelector('#korona');
-korona.appendChild("positif");
